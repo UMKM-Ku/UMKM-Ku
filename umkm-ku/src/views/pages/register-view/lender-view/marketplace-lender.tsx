c862e40432708631e877b77160c70500ea9e0f55
diananov11 <ohlemon0678@gmail.com>
2024-11-17 22:41:44 +0700

    add card

--- umkm-ku/src/views/pages/register-view/lender-view/marketplace-lender.tsx
+++ umkm-ku/src/views/pages/register-view/lender-view/marketplace-lender.tsx
@@ -1,41 +1,50 @@
+import CardMarketplace from "./card-marketplace";
 import FilterCheckbox from "./filter-checkbox";
 
 const MarketplaceLender = () => {
   return (
     <>
-      <aside
-        id="sidebar-multi-level-sidebar"
-        className="overflow-auto w-64 h-screen transition-transform  sm:translate-x-0 m-8"
-        aria-label="Sidebar"
-      >
-        <div className="h-full px-3 py-4 ">
-          <h1 className="text-2xl mb-3">Filters</h1>
-          <FilterCheckbox
-            title="Skor Kredit"
-            labelOne="A"
-            labelTwo="B"
-            labelThree="C"
-          />
-          <FilterCheckbox
-            title="Tenor"
-            labelOne="10 Minggu"
-            labelTwo="20 Minggu"
-            labelThree="30 Minggu"
-          />
-          <FilterCheckbox
-            title="Sektor"
-            labelOne="Perdagangan"
-            labelTwo="Pertanian"
-            labelThree="Peternakan"
-          />
-          <FilterCheckbox
-            title="Bagi Hasil"
-            labelOne="5% - 10%"
-            labelTwo="15% - 20%"
-            labelThree="25% - 30%"
-          />
+      <div className="md:flex gap-10">
+        <aside
+          id="sidebar-multi-level-sidebar"
+          className="overflow-auto w-64 h-screen transition-transform  sm:translate-x-0 m-8"
+          aria-label="Sidebar"
+        >
+          <div className="h-full px-3 py-4 ">
+            <h1 className="text-2xl mb-3">Filters</h1>
+            <FilterCheckbox
+              title="Skor Kredit"
+              labelOne="A"
+              labelTwo="B"
+              labelThree="C"
+            />
+            <FilterCheckbox
+              title="Tenor"
+              labelOne="10 Minggu"
+              labelTwo="20 Minggu"
+              labelThree="30 Minggu"
+            />
+            <FilterCheckbox
+              title="Sektor"
+              labelOne="Perdagangan"
+              labelTwo="Pertanian"
+              labelThree="Peternakan"
+            />
+            <FilterCheckbox
+              title="Bagi Hasil"
+              labelOne="5% - 10%"
+              labelTwo="15% - 20%"
+              labelThree="25% - 30%"
+            />
+          </div>
+        </aside>
+        <div className="grid md:grid-cols-2">
+          <CardMarketplace />
+          <CardMarketplace />
+          <CardMarketplace />
+          <CardMarketplace />
         </div>
-      </aside>
+      </div>
     </>
   );
 };
