2608fc11e2a9ba5c85a04405d0452ed2cf8122c5
BrahmantyoSP <brahmantyo59@gmail.com>
2024-11-17 21:24:09 +0700

    Edited carousel banner

--- umkm-ku/src/app/lender/page.tsx
+++ umkm-ku/src/app/lender/page.tsx
@@ -25,17 +25,52 @@ export default function Lender() {
         </div>
       </header>
 
-      {/* Banner */}
-      <section className="bg-green-100 rounded-lg p-4 flex justify-between items-center shadow-md mb-6">
-        <div>
-          <h2 className="text-xl font-bold text-green-700">
-            Yuk, danai para UMKM
-          </h2>
-          <p className="text-gray-600">
-            Mereka membutuhkan modal untuk berkembang.
-          </p>
+      {/* Carousel Banner */}
+      <section className="mb-6">
+        <div className="carousel w-full rounded-lg shadow-md">
+          <div id="item1" className="carousel-item w-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+              className="w-full"
+              alt="Banner 1"
+            />
+          </div>
+          <div id="item2" className="carousel-item w-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+              className="w-full"
+              alt="Banner 2"
+            />
+          </div>
+          <div id="item3" className="carousel-item w-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+              className="w-full"
+              alt="Banner 3"
+            />
+          </div>
+          <div id="item4" className="carousel-item w-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+              className="w-full"
+              alt="Banner 4"
+            />
+          </div>
         </div>
-        <button className="btn btn-success">Cek Sekarang</button>
+        <div className="flex w-full justify-center gap-2 py-2">
+          <a href="#item1" className="btn btn-xs">
+            1
+          </a>
+          <a href="#item2" className="btn btn-xs">
+            2
+          </a>
+          <a href="#item3" className="btn btn-xs">
+            3
+          </a>
+          <a href="#item4" className="btn btn-xs">
+            4
+          </a>
+        </div>
       </section>
 
       {/* Informasi Aset */}
