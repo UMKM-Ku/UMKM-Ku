9f4d1ca4479255a053d6174b557f2f88dc7cdcc9
diananov11 <ohlemon0678@gmail.com>
2024-11-17 19:46:31 +0700

    add layout for lender

---
+++ umkm-ku/src/app/lender/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/views/pages/footer";
+import Navbar from "@/views/pages/navbar";
+
+export default function BorrowerLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body className="antialiased">
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
