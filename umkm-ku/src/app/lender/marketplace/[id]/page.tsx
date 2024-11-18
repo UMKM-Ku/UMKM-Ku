56b030d44cc3f449fdaba7273b34864c8a45c38b
irwanbachtiar4 <irwanbachtiar4@gmail.com>
2024-11-18 20:28:42 +0700

    detail-marketplace

--- umkm-ku/src/app/lender/marketplace/[id]/page.tsx
+++ umkm-ku/src/app/lender/marketplace/[id]/page.tsx
@@ -1,4 +1,9 @@
+import DetailLender from "@/views/pages/lender-view/detail-view/detail-lender";
+
 export default function DetailMarketplace() {
-  return;
-  <></>;
+  return (
+    <>
+      <DetailLender />
+    </>
+  );
 }
