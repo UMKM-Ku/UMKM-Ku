612b80eec42eb83a39ea2f5625e22c2340f71ed7
challenge-repository <julioandarestu80@gmail.com>
2024-11-22 02:16:10 +0700

    Adding fetch borrower list pendanaan

--- umkm-ku/src/views/_types/index.ts
+++ umkm-ku/src/views/_types/index.ts
@@ -15,3 +15,21 @@ export type Funding = {
   createdAt: string;
   updatedAt: string;
 };
+
+export type FundingBorrower = {
+  id: number;
+  borrowerId: number;
+  title: string;
+  description: string;
+  image: string;
+  totalFund: number;
+  tenor: number;
+  returnRate: number;
+  fundingExpired: string;
+  sectorId: number;
+  status: number;
+  fundsRaised: number;
+  isFullyFunded: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
